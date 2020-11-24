499f68dc839f3d0a3a93a3c81059ada083730469
Forest <forest@cantaa.de>
2020-11-24 22:49:46 +0100

    Class 'VegetativeAnamnesis' created

---
+++ src/main/java/thb/siprojektanamneseservice/model/VegetativeAnamnesis.java
@@ -0,0 +1,31 @@
+package thb.siprojektanamneseservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class VegetativeAnamnesis {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private UUID id;
+
+    private boolean insomnia;
+    private boolean sleepDisorders;
+    private VegetativeAnamnesisDecision thirst;
+    private VegetativeAnamnesisDecision appetite;
+    private VegetativeAnamnesisDecision bowelMovement;
+    private VegetativeAnamnesisDecision urination;
+}
