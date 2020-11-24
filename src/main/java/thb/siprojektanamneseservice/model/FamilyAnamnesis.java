8f47062bd86d9c6680acd7ba22025dd8f02589ba
Forest <forest@cantaa.de>
2020-11-24 22:46:21 +0100

    Class 'FamilyAnamnesis' created

---
+++ src/main/java/thb/siprojektanamneseservice/model/FamilyAnamnesis.java
@@ -0,0 +1,26 @@
+package thb.siprojektanamneseservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FamilyAnamnesis {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private UUID id;
+
+    private PreExistingIllness father;
+    private PreExistingIllness mother;
+}
