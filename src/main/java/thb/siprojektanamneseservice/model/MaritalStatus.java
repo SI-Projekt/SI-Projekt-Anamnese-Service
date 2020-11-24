1e4ad01a5b553ce4fe7108ea0585cea0aa7612f2
Forest <forest@cantaa.de>
2020-11-24 22:57:05 +0100

    Constructor added in 'MaritalStatus' - Structure changed

--- src/main/java/thb/siprojektanamneseservice/model/MaritalStatus.java
+++ src/main/java/thb/siprojektanamneseservice/model/MaritalStatus.java
@@ -1,9 +1,15 @@
 package thb.siprojektanamneseservice.model;
 
 public enum MaritalStatus {
-    SINGLE,
-    PARTNERSCHAFT,
-    VERHEIRATET,
-    GESCHIEDEN,
-    VERWITWET;
+    SINGLE("single"),
+    PARTNERSCHAFT("civil_union"),
+    VERHEIRATET("married"),
+    GESCHIEDEN("divorced"),
+    VERWITWET("widowed");
+
+    private final String maritalStatus;
+
+    MaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
 }
