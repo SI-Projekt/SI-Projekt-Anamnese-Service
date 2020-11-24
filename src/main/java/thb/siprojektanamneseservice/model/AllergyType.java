8028e76970320b37f1bcd98a07a913e2b42acca7
Forest <forest@cantaa.de>
2020-11-24 22:54:54 +0100

    Constructor added in 'AllergyType' - Structure changed

--- src/main/java/thb/siprojektanamneseservice/model/AllergyType.java
+++ src/main/java/thb/siprojektanamneseservice/model/AllergyType.java
@@ -2,15 +2,21 @@ package thb.siprojektanamneseservice.model;
 
 
 public enum AllergyType {
-    ANTIBIOTIKA,
-    FRUCHTZUCKER,
-    NAHRUNGSMITTEL,
-    POLLEN,
-    ROENTGENKONTRASTMITTEL,
-    TIERHAAR,
-    LOKALANAESTHETIKA,
-    MILCHZUCKER,
-    HAUSSTAUB,
-    KONSERVIERUNGSSTOFFE,
-    VITAMIN_B;
+    ANTIBIOTIKA("antibiotics"),
+    FRUCHTZUCKER("fructose"),
+    NAHRUNGSMITTEL("foods"),
+    POLLEN("pollen"),
+    ROENTGENKONTRASTMITTEL("radio_opaque_substance"),
+    TIERHAAR("animal_hair"),
+    LOKALANAESTHETIKA("local_anaesthetics"),
+    MILCHZUCKER("lactose"),
+    HAUSSTAUB("House_dust"),
+    KONSERVIERUNGSSTOFFE("antidegradant"),
+    VITAMIN_B("vitamin_b");
+
+    private final String allergyType;
+
+    AllergyType(String allergyType) {
+        this.allergyType = allergyType;
+    }
 }
