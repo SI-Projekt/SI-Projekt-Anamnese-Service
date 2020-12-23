1b7d855b68c8d047f9d09b8a19878992e7c3d848
MistraForest <mistraf16@yahoo.fr>
2020-12-23 14:32:55 +0100

    MaritalStatus ApiConstant 3rd change

--- src/main/java/thb/siprojektanamneseservice/rest/ApiConstants.java
+++ src/main/java/thb/siprojektanamneseservice/rest/ApiConstants.java
@@ -13,4 +13,12 @@ public class ApiConstants {
     //Patient
     public static final String MARITAL_STATUS_ROOT = API + "/maritalStatuss";
     public static final String MARITAL_STATUS_ITEM = MARITAL_STATUS_ROOT + "/{maritalStatusId}";
+
+    //MedicationInTake
+    public static final String MEDICATION_IN_TAKE_ROOT = API + "/medication_in_takes";
+    public static final String MEDICATION_IN_TAKE_ITEM = MEDICATION_IN_TAKE_ROOT + "/{medicationInTakeId}";
+
+    //PreExistingIllnesses
+    public static final String PRE_EXISTING_ILLNESS_ROOT = API + "/preExistingIllnesses";
+    public static final String PRE_EXISTING_ILLNESS_ITEM = PRE_EXISTING_ILLNESS_ROOT + "/{preExistingIllnessId}";
 }
