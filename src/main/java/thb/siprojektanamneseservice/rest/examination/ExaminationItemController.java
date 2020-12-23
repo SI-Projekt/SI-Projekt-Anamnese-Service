de50fc7c690eedd4008c787e2356730aab01f04f
Juvniele <fokamnji@th-brandenburg.de>
2020-12-23 02:02:46 +0100

    endpoint for examination created

--- src/main/java/thb/siprojektanamneseservice/rest/examination/ExaminationItemController.java
+++ src/main/java/thb/siprojektanamneseservice/rest/examination/ExaminationItemController.java
@@ -2,10 +2,51 @@ package thb.siprojektanamneseservice.rest.examination;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import thb.siprojektanamneseservice.model.Examination;
+import thb.siprojektanamneseservice.rest.ApiConstants;
+import thb.siprojektanamneseservice.service.ExaminationService;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.UUID;
 
+@RestController
+@RequestMapping(ApiConstants.EXAMINATION_ITEM)
 public class ExaminationItemController {
 
     private static final Logger log = LoggerFactory.getLogger(ExaminationItemController.class);
+    private final ExaminationService examinationService;
 
+    @Autowired
+    public ExaminationItemController(ExaminationService examinationService) {
+        this.examinationService = examinationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Examination getOne(@PathVariable("examinationId") UUID examinationId){
+        log.info("Get the examination [id={}]", examinationId);
+        Examination examination = examinationService.getOne(examinationId);
+        log.info("Examination with [id={}] fetched", examinationId);
+
+        return examination;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void delete(@PathVariable("examinationId") UUID examinationId) {
+        log.info("Delete the examination [id={}]", examinationId);
+        examinationService.delete(examinationId);
+        log.info("Examination with  [id={}] deleted", examinationId);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void update(@PathVariable("examinationId") UUID examinationId, @RequestBody Examination examination) {
+        log.info("Update the examination [id={}]", examinationId);
+        examinationService.update(examinationId, examination);
+        log.info("Examination with [id={}] updated", examinationId);
+    }
 }
--- src/main/java/thb/siprojektanamneseservice/rest/examination/ExaminationRootController.java
+++ src/main/java/thb/siprojektanamneseservice/rest/examination/ExaminationRootController.java
@@ -2,11 +2,51 @@ package thb.siprojektanamneseservice.rest.examination;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import thb.siprojektanamneseservice.model.Examination;
+import thb.siprojektanamneseservice.rest.ApiConstants;
+import thb.siprojektanamneseservice.service.ExaminationService;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
 
+@RestController
+@RequestMapping(ApiConstants.EXAMINATION_ROOT)
 public class ExaminationRootController {
 
     private static final Logger log = LoggerFactory.getLogger(ExaminationRootController.class);
 
+    private final ExaminationService examinationService;
 
+    @Autowired
+    public ExaminationRootController(ExaminationService examinationService) {
+        this.examinationService = examinationService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Examination create(@RequestBody Examination newExamination){
+        log.info("create an examination");
+        Examination created = examinationService.create(newExamination);
+        log.info("Examination created");
+
+        return created;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Examination> listAll(){
+        log.info("List all examinations");
+        List<Examination> examinations = examinationService.listAll();
+        log.info("Examinations list fetched");
+
+        return examinations;
+    }
 }
