1078a0ae7c60955285c939b5b5956585de396f3c
MistraForest <mistraf16@yahoo.fr>
2021-01-17 16:42:19 +0100

    Fix artifact deployment in Ec2

---
+++ scripts/startappsilent.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Check java command exists, discard output
+testJava=`which java > /dev/null 2>&1`
+
+# if the last command failed, install java
+if [[ $? -eq 1 ]] ; then
+	echo 'Java not install, Installing java'
+	yum -y install java-1.8.0-openjdk
+fi
+echo 'Run silently the runapp script'
+scriptDir="$(dirname "${BASH_SOURCE[0]}")"
+chmod +x "${scriptDir}/runapp.sh"
+nohup "${scriptDir}/runapp.sh" > /var/log/si-project.log 2>&1 &
