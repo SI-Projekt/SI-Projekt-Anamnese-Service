abec0646b62a988f9b4c0359672c9d3e9f42713a
MistraForest <mistraf16@yahoo.fr>
2021-01-19 19:09:07 +0100

    Convert ec2-tag to env; setup utilities.sh

---
+++ scripts/utilities.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# install jq for parsing json
+if [ -e /usr/bin/jq ]; then
+  echo "jq installed";
+else
+  echo "Installing jq";
+  sudo yum -y install jq
+fi
+
+# install dos2unix to enable aws recognizing script as such
+if [ -e /usr/bin/dos2unix ]; then
+  echo "dos2unix installed";
+else
+  echo "Installing dos2unix";
+  sudo yum -y install dos2unix
+fi
+scriptDir="$(dirname "${BASH_SOURCE[0]}")"
+find ${scriptDir} -name "*.sh" | xargs dos2unix
