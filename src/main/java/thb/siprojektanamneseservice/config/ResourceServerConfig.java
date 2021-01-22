02891286317fbaa7e4f4a77bb564ea7facdcde5e
MistraForest <mistraf16@yahoo.fr>
2021-01-22 07:07:12 +0100

    Handel cors issue

--- src/main/java/thb/siprojektanamneseservice/config/ResourceServerConfig.java
+++ src/main/java/thb/siprojektanamneseservice/config/ResourceServerConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -17,5 +19,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://si-project-webapp-bucket.s3-website-us-east-1.amazonaws.com")
+                .allowedMethods("GET");
     }
 }
